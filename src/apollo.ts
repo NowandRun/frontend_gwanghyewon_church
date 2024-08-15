400889f4bc51facfa64b69687e8640003ab39343
Jonghyeon Choi <whd617@naver.com>
2024-08-16 03:01:12 +0900

    chore/배포를 위한 codegen.ts 파일에서 docker-compose의 container의 이름으로 된 docker container통신을 위한 수정

--- src/apollo.ts
+++ src/apollo.ts
@@ -16,7 +16,7 @@ export const isLoggedInAccessTokenVar = makeVar(Boolean(accessToken)); // 초기
 export const authAccessToken = makeVar(accessToken);
 
 const httpLink = createHttpLink({
-  uri: 'http://backend:4000/graphql',
+  uri: 'https://wavenexus.co.kr/graphql',
   credentials: 'include',
 });
 
