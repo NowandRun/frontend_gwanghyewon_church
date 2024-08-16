7296931c76921d297df0899156632ba366de29c6
Jonghyeon Choi <whd617@naver.com>
2024-08-16 19:14:13 +0900

    chore/배포를 위한 codegen과 apollo.ts 변경

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'https://backend/graphql',
+  schema: 'https://wavenexus.co.kr/graphql',
   documents: ['src/**/*.tsx'],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
