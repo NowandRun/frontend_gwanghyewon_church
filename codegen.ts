c753e6c65398e864c8e64e9fccde1d0947708227
Jonghyeon Choi <whd617@naver.com>
2024-08-16 23:42:12 +0900

    chore/배포를 위한 수정

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'https://wavenexus.co.kr/graphql',
+  schema: 'https://backend:4000/graphql',
   documents: ['src/**/*.tsx'],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
