1d69acadefe39900b39e5bd4b16bab35372973e6
Jonghyeon Choi <whd617@naver.com>
2024-08-17 21:39:44 +0900

    chore/배포를 위한 apollo와 codegen uri 수정

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'http://nginx/graphql',
+  schema: 'https://nginx/graphql',
   documents: ['src/**/*.tsx'],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
