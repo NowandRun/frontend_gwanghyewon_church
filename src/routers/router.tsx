29ed8b143fe58c1ef36f985773e2268712476d21
Jonghyeon Choi <chlwhdgus617@gmail.com>
2025-08-23 21:02:57 +0900

    chore/git-pages배포를 위한 Router 변경

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -1,4 +1,5 @@
-import { createBrowserRouter } from 'react-router-dom';
+// 변경
+import { createHashRouter } from 'react-router-dom';
 import { generateRoutes } from './generateRoutes';
 import React from 'react';
 import Root from "../Root";
@@ -7,7 +8,7 @@ import { menuItems } from "../components/Header/Header";
 
 const generatedRoutes = generateRoutes(menuItems);
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <Root />,
