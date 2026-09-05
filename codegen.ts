048055aa6530d7a4458d343def4cd0f4a3595fbd
wavenexus-enterprice <wavenexus001@gmail.com>
2026-09-05 09:24:37 +0900

    fix/홈서버 배포를 위한 환경변수 수정 및 백엔드 통신 변경

--- codegen.ts
+++ codegen.ts
@@ -1,19 +1,23 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
-/* 'https://wavnexus.co.kr/graphql' */
-/* http://localhost:4000/graphql */
+import * as dotenv from 'dotenv';
+
+// ✅ .env 파일의 환경변수 로드
+dotenv.config();
+
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'http://localhost:4000/graphql',
+  // .env 파일에 작성된 REACT_APP_GRAPHQL_URI 또는 GRAPHQL_SCHEMA_URL 로드
+  schema: process.env.GRAPHQL_SCHEMA_URL || 'http://localhost:4000/graphql',
   documents: ['src/**/*.tsx'],
-  ignoreNoDocuments: true, // for better experience with the watcher
+  ignoreNoDocuments: true,
   generates: {
     './src/gql/': {
       preset: 'client',
       presetConfig: {
-        //fragmentMasking: false 로 두면 fragment로 지정한부분을 $fragmentRef라는 필드로 안감싸게 해주는 옵션
         fragmentMasking: false,
       },
     },
   },
 };
+
 export default config;
